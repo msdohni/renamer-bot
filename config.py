87d53a2ebad057a949521f0ebbd6f404ce661847
msdohni <141110310+msdohni@users.noreply.github.com>
2023-12-17 00:36:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "26579601")
 API_HASH = os.environ.get("API_HASH", "5b811e9e89adfe6c636d3328b247779d")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw") 
-#TOKEN_ONE = os.environ.get("TOKEN_ONE", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw")
+#BOT_TOKEN = os.environ.get("BOT_TOKEN", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw") 
+TOKEN_ONE = os.environ.get("TOKEN_ONE", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw")
 
 CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
 BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
