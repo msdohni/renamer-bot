0b7c0d8d47d041faabfd216493be35ae86469f40
msdohni <141110310+msdohni@users.noreply.github.com>
2023-12-17 00:31:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "26579601")
 API_HASH = os.environ.get("API_HASH", "5b811e9e89adfe6c636d3328b247779d")
-#BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-TOKEN_ONE = os.environ.get("TOKEN_ONE", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw") 
+#TOKEN_ONE = os.environ.get("TOKEN_ONE", "5402380823:AAGMW-Fqp_U8jrQJlaeZOn4l6aCUDYrnbhw")
 
 CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
 BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
@@ -20,5 +20,5 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://stautofilesbot:stautofilesbot@cl
 FLOOD = int(os.environ.get("FLOOD", "90"))
 LAZY_PIC = os.environ.get("LAZY_PIC", "")
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6838589973').split()]
-PORT = os.environ.get('PORT', '8080')
+PORT = os.environ.get('PORT', '80')
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001790795488"))
